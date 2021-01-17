100ab28ee3bab927316fc4ec76f9192212568dfd
YOSSI263 <46202069+YOSSI263@users.noreply.github.com>
2021-01-17 15:57:18 +0200

    Add files via upload

---
+++ ip.php
@@ -0,0 +1,113 @@
+﻿<head>
+<html lang="he">
+    <meta charset="utf-8" />
+    <title>דאטה ip | אביי</title>
+    <link rel="shortcut icon" href="https://mitmachim.top/assets/uploads/profile/6-profileavatar-1566251651743.png">
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-163311335-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-163311335-1');
+</script>
+
+</head>
+
+<body lang=he-il link="#0563C1" vlink="#954F72" style='tab-interval:36.0pt'>
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:right'><b><span lang=HE style='font-size:6.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:white'>בס"ד עודכן לאחרונה 10.04.20</span></b><b><span lang=HE style='font-size:20.0pt;line-height:
+107%;font-family:"Arial",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:#00B050'><o:p></o:p></span></b></p>
+
+<div align="center" style="position: relative; top: 3%; left: 0px;">
+<a href="http://abaye.xyz/">
+<img src="C:\Users\אביי\Desktop\IMG_2582.jpg" width="130" height="130" alt="logo">
+</a>
+</div>
+
+<style>body  { background: blue }</style>
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:center'><b><span lang=HE style='font-size:48.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:white;mso-highlight:
+yellow'>מה כתובת הip שלי?</span></b><b><span lang=HE style='font-size:34.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:fbff08'><o:p></o:p></span></b></p>
+
+<p class=MsoNormal align=center dir=RTL style='text-align:center'><span
+lang=HE style='font-family:"Arial",sans-serif;mso-ascii-font-family:Calibri;
+mso-ascii-theme-font:minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:
+minor-latin;mso-bidi-theme-font:minor-bidi'> <o:p></o:p></span></p>
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:center'><b><span lang=HE style='font-size:32.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:white;mso-highlight:
+yellow'>זוהי כתובת הip שלך:</span></b><b><span lang=HE style='font-size:28.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:fbff08'><o:p></o:p></span></b></p>
+
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:center'><b><span lang=HE style='font-size:16.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:white'>השימוש בכפוף להוראות החוק!</span></b><b><span lang=HE style='font-size:20.0pt;line-height:
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:center'><span lang=HE style='font-size:28.0pt;line-height:
+107%;font-family:Calibri'><o:p>&nbsp;</o:p></span></p>
+
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:center'><b><span lang=HE style='font-size:22.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:RED;background-color:white'>ליצירת קשר:</span></b><b><span lang=HE style='font-size:20.0pt;line-height:
+107%;font-family:"Arial",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:#00B050'><o:p></o:p></span></b></p>
+
+</div>
+
+
+<style>
+a:link, a:visited {
+
+  color: white;
+  padding: 15px 25px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+}
+
+a:hover, a:active {
+
+}
+</style>
+
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:center'><span lang=HE style='font-family:"calibri",sans-serif;
+mso-ascii-font-family:Calibri;mso-ascii-theme-font:minor-latin;mso-hansi-font-family:
+Calibri;mso-hansi-theme-font:minor-latin;mso-bidi-theme-font:minor-bidi;color:white'>אימייל:<a href="mailto:8140hp@gmail.com?subject=היי,%20אביי,">8140hp@gmail.com</a></span></p>
+
+
+<p class=MsoNormal align=center dir=RTL style='margin-bottom:0cm;margin-bottom:
+.0001pt;text-align:center'><b><span lang=HE style='font-size:16.0pt;line-height:
+107%;font-family:"Calibri",sans-serif;mso-ascii-font-family:Calibri;mso-ascii-theme-font:
+minor-latin;mso-hansi-font-family:Calibri;mso-hansi-theme-font:minor-latin;
+mso-bidi-theme-font:minor-bidi;color:white'>דף זה הינו חלק ממאגר דאטה ip השלם | © כל הזכויות שמורות @אביי</span>
